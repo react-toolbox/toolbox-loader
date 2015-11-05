3106a1afb0a71514d8847e377cab021af0f95d6e
Javi Velasco <javier.velasco86@gmail.com>
2015-11-05 20:57:51 +0100

    Proper load path for colors sass file

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-var THEMED = '@import "~react-toolbox/_colors"; \n $theme-building: true;';
+var HEADING = '@import "~react-toolbox/lib/_colors"; \n $theme-building: true;';
 var DEFAULT_NAME = 'theme.scss';
 var path = require('path');
 var fs = require('fs');
 
 module.exports = function (source) {
   if (this.cacheable) this.cacheable();
-  var callback = this.async();
-  var themeName = this.options.toolboxTheme || DEFAULT_NAME;
+  var callback  = this.async();
+  var options   = this.options.toolbox || {};
+  var fromBuild = options.fromBuild || true;
+  var themeName = options.theme || DEFAULT_NAME;
   var themePath = path.resolve(themeName);
+  var heading = HEADING;
+
+  if (!fromBuild) {
+    heading = heading.replace('/lib', '');
+  }
 
   this.addDependency(themePath);
 
   fs.readFile(themePath, "utf-8", function(err, theme) {
     if(err) return callback(err);
-    callback(null, THEMED + '\n' + theme + '\n' + source);
+    callback(null, heading + '\n' + theme + '\n' + source);
   });
 };
